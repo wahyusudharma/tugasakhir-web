46435b545d7003fd0fae0e5c9608ef5065f15e22
benartdo <wbenartdo@gmail.com>
2024-06-05 12:53:01 +0800

    first commit

---
+++ example-app/app/Models/password_resets.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class password_resets extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'new_password',
+    ];
+}
